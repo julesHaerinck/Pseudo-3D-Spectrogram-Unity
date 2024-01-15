924364744f3eba838f4d31c3984fa987e801c11b
julesHaerinck <haerinck.jules@gmail.com>
2024-01-15 23:01:51 +0100

    Removed the code in charge of moving the camera.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -10,7 +10,7 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Target);
-        transform.Translate(Vector3.right * Speed * Time.deltaTime);
+        //transform.LookAt(Target);
+        //transform.Translate(Vector3.right * Speed * Time.deltaTime);
     }
 }
